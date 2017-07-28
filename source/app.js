04613a4f35ad96e8f8daa40f92acfcc1084daf8f
RESONEO Google Analytics <ovit.resoneo@gmail.com>
2017-07-28 14:11:59 +0200

    force SSL and code formatting
    better formatting allowing code review=> this social code tracking isn't compatible with analytics.js

--- source/app.js
+++ source/app.js
@@ -1,12 +1,39 @@
 (function () {
-  var options = INSTALL_OPTIONS
-
-  if (!options.id) return
-
-  window.ga('create', options.id, 'auto')
-  window.ga('send', 'pageview')
-
-  if (options.social){
-    (function(b){(function(a){"__CF"in b&&"DJS"in b.__CF?b.__CF.DJS.push(a):"addEventListener"in b?b.addEventListener("load",a,!1):b.attachEvent("onload",a)})(function(){"FB"in b&&"Event"in FB&&"subscribe"in FB.Event&&(FB.Event.subscribe("edge.create",function(a){_gaq.push(["_trackSocial","facebook","like",a])}),FB.Event.subscribe("edge.remove",function(a){_gaq.push(["_trackSocial","facebook","unlike",a])}),FB.Event.subscribe("message.send",function(a){_gaq.push(["_trackSocial","facebook","send",a])}));"twttr"in b&&"events"in twttr&&"bind"in twttr.events&&twttr.events.bind("tweet",function(a){if(a){var b;if(a.target&&a.target.nodeName=="IFRAME")a:{if(a=a.target.src){a=a.split("#")[0].match(/[^?=&]+=([^&]*)?/g);b=0;for(var c;c=a[b];++b)if(c.indexOf("url")===0){b=unescape(c.split("=")[1]);break a}}b=void 0}_gaq.push(["_trackSocial","twitter","tweet",b])}})})})(window);
-  }
-}())
+        var options = INSTALL_OPTIONS
+        if (!options.id) return
+        window.ga('create', options.id, 'auto');
+        window.ga('set', 'forceSSL', true); /* prevents 307 redirect from GA since secure request are now required */
+        window.ga('send', 'pageview');
+        if (options.social) {
+            (function (b) {
+                (function (a) {
+                    "__CF"in b && "DJS"in b.__CF ? b.__CF.DJS.push(a) : "addEventListener"in b ? b.addEventListener("load", a, !1) : b.attachEvent("onload", a)
+                })(function () {
+                    "FB"in b && "Event" in FB && "subscribe" in FB.Event && (FB.Event.subscribe("edge.create", function (a) {
+                        _gaq.push(["_trackSocial", "facebook", "like", a])
+                    }), FB.Event.subscribe("edge.remove", function (a) {
+                        _gaq.push(["_trackSocial", "facebook", "unlike", a])
+                    }), FB.Event.subscribe("message.send", function (a) {
+                        _gaq.push(["_trackSocial", "facebook", "send", a])
+                    }));
+                    "twttr"in b && "events"in twttr && "bind"in twttr.events && twttr.events.bind("tweet", function (a) {
+                        if (a) {
+                            var b;
+                            if (a.target && a.target.nodeName == "IFRAME")a:{
+                                if (a = a.target.src) {
+                                    a = a.split("#")[0].match(/[^?=&]+=([^&]*)?/g);
+                                    b = 0;
+                                    for (var c; c = a[b]; ++b)if (c.indexOf("url") === 0) {
+                                        b = unescape(c.split("=")[1]);
+                                        break a
+                                    }
+                                }
+                                b = void 0
+                            }
+                            _gaq.push(["_trackSocial", "twitter", "tweet", b])
+                        }
+                    })
+                })
+            })(window);
+        }
+    }())
